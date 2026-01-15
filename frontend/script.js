bf0e16c6ccd7ed330fbf434fb30cb5d90feaa789
mitvr006 <pawankumar3439744@gmail.com>
2026-01-15 20:33:54 +0530

    css styling and text change

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,8 @@
-document.getElementById("btn").onclick = function () {
-  alert("Button Clicked!");
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+
+btn.onclick = function () {
+  text.innerText = "Button Click Ho Gaya!";
+  text.style.color = "green";
+
 };
