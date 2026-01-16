9d299a235ea2a0031f1a062450b0faa69643c307
mitvr006 <pawankumar3439744@gmail.com>
2026-01-16 20:09:03 +0530

    javascript variables and functions

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,28 @@
+// const btn = document.getElementById("btn");
+// const text = document.getElementById("text");
+
+// btn.onclick = function () {
+//   text.innerText = "Button Click Ho Gaya!";
+//   text.style.color = "green";
+
+// };
+
+
+// variables
+let count = 0;
+
+// elements
 const btn = document.getElementById("btn");
 const text = document.getElementById("text");
 
-btn.onclick = function () {
-  text.innerText = "Button Click Ho Gaya!";
-  text.style.color = "green";
+// function
+function increaseCount() {
+  count = count + 1;
+  text.innerText = "Button " + count + " bar click hua";
+  //text.innerText = text.innerText.toUpperCase();
 
-};
+}
+
+// event
+btn.onclick = increaseCount;
+
