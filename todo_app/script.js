77eea798793519dd6617ae3e9e411e9f4655360d
mitvr006 <pawankumar3439744@gmail.com>
2026-01-19 20:11:53 +0530

    todo app with localstorage

--- todo_app/script.js
+++ todo_app/script.js
@@ -2,6 +2,36 @@ const input = document.getElementById("todoInput");
 const addBtn = document.getElementById("addBtn");
 const list = document.getElementById("todoList");
 
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+// page load pe tasks show karo
+function showTodos() {
+  list.innerHTML = "";
+
+  todos.forEach(function (task, index) {
+    const li = document.createElement("li");
+    li.innerText = task;
+
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+
+    delBtn.onclick = function () {
+      todos.splice(index, 1);
+      saveTodos();
+    };
+
+    li.appendChild(delBtn);
+    list.appendChild(li);
+  });
+}
+
+// save function
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+  showTodos();
+}
+
+// add task
 addBtn.onclick = function () {
   const task = input.value;
 
@@ -10,25 +40,11 @@ addBtn.onclick = function () {
     return;
   }
 
-  const li = document.createElement("li");
-  li.innerText = task;
-
-  const delBtn = document.createElement("button");
-  delBtn.innerText = "Delete";
-
-  delBtn.onclick = function () {
-    li.remove();
-  };
-
-  li.appendChild(delBtn);
-  list.appendChild(li);
-
+  todos.push(task);
+  saveTodos();
   input.value = "";
-
-  input.addEventListener("keypress", function(e) {
-  if (e.key === "Enter") {
-    addBtn.click();
-  }
-});
-
 };
+
+// first load
+showTodos();
+
